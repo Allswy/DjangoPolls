7c4f57862bd316f582a0c883156c8b08255d5716
Allswy <swyswy123@outlook.com>
2025-12-03 15:46:46 +0800

    learned

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 from . import views
+app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ 返回最近时间的五个问题 """
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    choices = Choice.objects.filter(question=question)
-    return render(request, 'polls/detail.html', {'question': question, 'choices': choices})
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    response = "You're looking at the result for question %s" % question_id
-    return HttpResponse(response)
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting for question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 重新显示问题的投票表单
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功处理之后 POST 数据之后，总是返回一个 HttpResponseRedirect 。防止因为用户点击了后退按钮而提交了两次。
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
